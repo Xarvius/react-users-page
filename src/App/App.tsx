6428a9fd48119b7d11692d57c6ca03811db25cfc
Xarvius <strzelecki@gmail.com>
2020-11-02 13:23:18 +0100

    Move routes to own files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import Routes from './routes'
+// import Routes from './routes'
 import logo from './assets/logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap'
+import Home from './containers/Home'
+import UsersList from './containers/UsersList'
 
 export default function App() {
   return (
     <div className="App">
-      <Routes />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="users">UsersList</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UsersList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/App/containers/UsersList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UsersList() {
+    return (
+      <div>
+        <h2>UsersList</h2>
+      </div>
+    );
+  }
