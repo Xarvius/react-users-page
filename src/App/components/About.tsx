d4571546ad64b44f827c6bb5eec302ca486604a4
Xarvius <strzelecki@gmail.com>
2021-01-04 13:18:58 +0100

    extend about and files

--- src/App/components/About.tsx
+++ src/App/components/About.tsx
@@ -28,31 +28,46 @@ const GET_EXTRAS = gql`
               education
             }
   }`;
-
-
+const smallEducationTitle = (title: string) => {
+  return title.toLowerCase() + "."
+}
 
 export default function UsersList(props: {profile?: propsObject}) {
     const [basicInfo] = useState(props);
     const { loading, data, error } = useQuery<{extras: extrasResp}>(GET_EXTRAS, {
       variables: {id: basicInfo?.profile?.user.id},
     });
-
-    if (loading || error) return (
-    <div>
+    if (error) {
+      return (
+      <div>
+        <Redirect to="/users" />
+      </div>)}
+    else if (loading) {
+      return (
+      <div>
         "loading"
-    </div>)
+      </div>)}
     return (
       <div>
-        <Col>
-        <Card className="bg-dark text-white">
-            <h2>
-                <Image src="" roundedCircle />
-                {basicInfo.profile?.firstName} {basicInfo.profile?.lastName} 
+        <div className="media bg-dark text-white">
+          <img className="media-left"  alt="Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGifDT7EaGssG2GNBWmpx-nDk73RdHHLWhWyB8BsflBPfjUqX8uGoUYVK7_woaGBClPEIaoPbmIme92qzNPGK-RB_WE6izhYE&usqp=CAU&ec=45725305" />
+          <div className="media-body">
+            <h2 className="card-title">
+              {smallEducationTitle(basicInfo.profile!.qualification)} {basicInfo.profile?.firstName} {basicInfo.profile?.lastName}
             </h2>
-            <p>{ data?.extras?.intro }</p>
+            <p className="card-text">{ data?.extras?.intro }</p>
+          </div>
+        </div>
+        <Card className="bg-dark text-white education">
+        <Row>
+          <Col sm={10}>
+            { data?.extras?.education }
+          </Col>
+          <Col sm={2}>
+            <Book size={85} />
+            </Col>
+        </Row>
         </Card>
-        </Col>
-        
         <Publications id={basicInfo?.profile?.user.id} />
       </div>
     );
--- src/App/components/Files.tsx
+++ src/App/components/Files.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useQuery, gql } from '@apollo/client';
 import { FileText } from 'react-bootstrap-icons'
 
@@ -14,26 +14,36 @@ query getFiles($id: ID!){
             file
             }
 }`;
+const fileUrl = (path: string) => {
+    const url = path.split("/")
+    console.log(url[1] + url[2])
+    return "http://localhost:8000/" + url[1] + "/" + url[2]
+}
+
 const fileName = (path: string) => {
     return path.split("/").pop()
 }
 
 export default function FilesList(props: {id?: string}) {
-    const [folderID] = useState(props);
+    const [folderID, updateFolderID] = useState(props);
     const { loading, data, error } = useQuery(GET_FILES, {
         variables: folderID,
     });
+    useEffect(() => {
+        updateFolderID(props);
+      }, [props]);
+
     if (loading || error) return (
         <div>
             "loading"
         </div>)
     return (
-        <div>
-          {
+        <div className="file">
+          {data.files.length !== 0 ?
             data?.files?.map((file: filesResponse) => (
-                <p key={file.id}><FileText />  {fileName(file.file)}</p>
+            <p key={file.id}><FileText size={35} /> <a href={fileUrl(file.file)} target="_blank" rel="noopener noreferrer"> {fileName(file.file)} </a></p>
             ))
-          }
+          : "Brak plików"}
         </div>
       );
 }
